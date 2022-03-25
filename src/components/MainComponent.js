9a7f4bdca52d1a029e96e27ef076b92e7ecb73a3
Daniel Graham <dgraham.sprint@gmail.com>
2022-03-24 20:14:14 -0500

    Initial Setup

---
+++ src/components/MainComponent.js
@@ -0,0 +1,22 @@
+import React, {Component}  from 'react';
+
+class Main extends Component {
+
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+
+     
+
+        return(
+      <div>
+       Test
+     </div>
+        )
+
+    }
+}
+
+export default Main;
