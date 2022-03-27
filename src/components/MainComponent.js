a3964842563c4f58ce3cfc926fad7e26f399ba09
Daniel Graham <dgraham.sprint@gmail.com>
2022-03-27 12:52:19 -0500

    Fixed bootstrap, initial steps

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, {Component}  from 'react';
-import { Card } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 class Main extends Component {
 
@@ -12,49 +13,56 @@ class Main extends Component {
      
 
         return(
-            <div>
-            <div className="container">
-              <div className='row justify-content-center p-3'>
-                <Card className='p-1'><h1>Test</h1></Card>
-              </div>
-              
-                 <div className="row mx-auto">           
-                        <div className='col-lg'>                     
-                        <Card>
-                        <Card.Header ><h3 className='p-1'>Test</h3></Card.Header>
-                          <Card.Body>
-                              Test
-                          </Card.Body>
-                        </Card>
-                        </div>
-
-                        <div className="col">
-                        <Card>
-                              <Card.Header>
-                                <h2 className='p-2'>Test</h2>
-                               </Card.Header>
-    
-                          <Card.Body>                      
-                              Test
-                          </Card.Body>       
-                          </Card>
-                      </div>
-    
-                      <div className='col-lg-4'>
-                      <Card>
-                        <Card.Header><h2 className='p-2'>Test</h2></Card.Header>
-                        <Card.Body>
-                            Test
-                        </Card.Body>
-                      </Card>
-                      </div>
-    
-    
-    
-              </div>
-    
+        <div>
+        <div className='container p-5'>
+            <div className='row'>
+            <div className='col'>
+            <CardGroup>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
             </div>
-         </div>
+            </div>
+        </div>
+        </div>
         )
 
     }
