c217a3a130613db1f1d873c1d9b094528aaa5dd5
Daniel Graham <dgraham.sprint@gmail.com>
2022-03-27 18:50:33 -0500

    Templating initial design.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, {Component}  from 'react';
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
+import Button from 'react-bootstrap/Button'
 
 class Main extends Component {
 
@@ -8,23 +9,44 @@ class Main extends Component {
     constructor(props) {
         super(props);
     }
+
+
     render() {
 
      
 
         return(
+
         <div>
         <div className='container p-5'>
+          <div className='row d-flex justify-content-center mb-5'>
+            <Card>
+              <Card.Title className='d-flex justify-content-center p-3'><h1>Story Title</h1></Card.Title>
+              <Card.Body className='d-flex justify-content-center'>(Pulled from database)</Card.Body>
+              <Card.Footer className='d-flex justify-content-center'>Pick your next steps..</Card.Footer>
+            </Card>
+          </div>
             <div className='row'>
             <div className='col'>
             <CardGroup>
   <Card>
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>Dynamic Option 1</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+        (Import choice one)
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer className='d-flex justify-content-center'>
+      <Button>Choice 1</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Dynamic Option 2</Card.Title>
+      <Card.Text>
+        (Import choice 2)
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -34,20 +56,7 @@ class Main extends Component {
   <Card>
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
+      <Card.Title>Write you own choice</Card.Title>
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This card has even longer content than the first to
