f64ec781e01882cf7c86cab742bce7b8af87f1e2
Daniel Graham <dgraham.sprint@gmail.com>
2022-04-16 22:14:18 -0500

    Starting over

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -15,16 +15,6 @@ class Main extends Component {
         return(
 
         <div>
-        <div className='container p-5'>
-          <div className='row d-flex justify-content-center mb-5'>
-          Test
-          </div>
-            <div className='row'>
-            <div className='col'>
-           Test
-            </div>
-            </div>
-        </div>
         </div>
         )
 
